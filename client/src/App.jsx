b97744427e28c4b0e44a749d0c2c45613c708a9c
Nasna <nasnaim19@email.com>
2026-07-24 16:58:11 +0000

    Add authentication pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Cuisines from "./pages/Cuisines";
 import Recipes from "./pages/Recipes";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Home */}
         <Route path="/" element={<Home />} />
+
+        {/* Cuisines */}
         <Route path="/cuisines" element={<Cuisines />} />
+
+        {/* Recipes */}
         <Route path="/recipes" element={<Recipes />} />
+
+        {/* Authentication */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
